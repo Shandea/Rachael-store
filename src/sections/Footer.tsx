66f8888fc99e6a0d1bdd0c0326c7a096b1e7516d
tfortner_banyanLabs <212409227+tfortner-banyanlabs@users.noreply.github.com>
2025-06-06 09:25:20 -0500

    fixes

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,7 +4,7 @@ import Button from '../components/ui/Button';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 text-gray-700 px-6 py-10 border-t w-full mt-auto">
+    <footer id='footer' className="bg-gray-50 text-gray-700 px-6 py-10 border-t w-full mt-auto">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center md:items-start gap-8">
         <div className="flex flex-col items-start w-full md:w-auto">
           <Heading level={4} text="ShoeStyle" className="text-gray-900" />
