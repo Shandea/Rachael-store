aace0464f324f29f10823a24a380a2a3e8dbe68c
tfortner_banyanLabs <212409227+tfortner-banyanlabs@users.noreply.github.com>
2025-06-06 12:11:13 -0500

    bugs

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,17 +4,17 @@ import Button from '../components/ui/Button';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 text-gray-700 px-6 py-10 border-t w-full mt-auto">
+    <footer id='footer' className="bg-gray-50 text-gray-700 px-6 py-10 border-t w-full mt-auto">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center md:items-start gap-8">
         <div className="flex flex-col items-start w-full md:w-auto">
           <Heading level={4} text="ShoeStyle" className="text-gray-900" />
           <hr className="my-4 w-full border-gray-700" />
-          <Paragraph variant="small" className="text-gray-400">
+          <Paragraph variant="small" className="text-gray-700">
             © {new Date().getFullYear()} ShoeStyle. All rights reserved.
           </Paragraph>
         </div>
 
-        <nav className="flex flex-wrap justify-center md:justify-start gap-4 text-sm text-gray-500">
+        <nav className="flex flex-wrap justify-center md:justify-start gap-4 text-sm text-gray-500 text-underlineNone">
           <Button variant="link" size="sm" onClick={() => window.location.href='/about'}>
             About
           </Button>
@@ -31,8 +31,8 @@ const Footer = () => {
       </div>
 
       <div className="mt-6 max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center text-sm">
-        <span></span>
-        <div className="flex gap-4 text-sky-300">
+        <span className='w-full text-gray-500 '></span>
+        <div className="flex gap-4 text-gray-500 text-underlineNone">
           <Button variant="link" size="sm" onClick={() => window.location.href='/privacy'}>
             Privacy
           </Button>
